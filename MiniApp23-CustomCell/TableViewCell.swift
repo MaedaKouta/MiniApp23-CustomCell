a8a56febd6d2e927e689cdbdba107f25df0ebcaa
MaedaKouta <maedakoutaa@gmail.com>
2022-03-05 14:40:35 +0900

    commit

---
+++ MiniApp23-CustomCell/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  MiniApp23-CustomCell
+//
+//  Created by 前田航汰 on 2022/03/05.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
+
+}
--- MiniApp23-CustomCell/ViewController.swift
+++ MiniApp23-CustomCell/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
+        cell.leftLabel.text = "\(indexPath.row)"
+        cell.rightLabel.text = "\(indexPath.row)"
+        
+        return cell
+    }
 }
 
